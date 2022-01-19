02cf44db400d6fbf899f4b90255ac784289e0015
Leonardo <leonardo.mf@ufu.br>
2022-01-19 20:06:39 -0300

    lab03 update

--- Lab03 - Data Structures/list/List.java
+++ Lab03 - Data Structures/list/List.java
@@ -1,20 +1,42 @@
 package list;
 
+import list.Node.Node;
+
 public class List {
     private Node head;
+    private Node tail;
+    private int size;
 
-    public void List(){
+    public List(){
         head = null; // ?
+        tail = null;
+        size = 0;
     }
 
     public void push_front(int valor){
-        if(head != null){
+        if(head == null){
+            head = new Node(valor);
+            tail = head;
+            size++;
+        }else{
+            Node newNode = new Node(valor);
+            tempNode.setNextNode(head);
+            head = newNode;
+            size++;
         }
     }
 
     public void push_back(int valor){
-
+        if(size == 0){
+            push_front(valor);
+        }else{
+            Node newNode = new Node(valor);
+            tail.setNextNode(newNode);
+            tail = tail.getNext();
+            size++;
+        }
     }
 
+    
     
 }
