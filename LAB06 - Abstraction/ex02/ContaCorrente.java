4710464d06c5043012f5e70320d0e66dc71b444c
Leonardo <leonardo.mf@ufu.br>
2022-02-10 17:23:48 -0300

    lab06 diretorio ex02 add

--- LAB06 - Abstraction/ex02/ContaCorrente.java
+++ LAB06 - Abstraction/ex02/ContaCorrente.java
@@ -1,11 +1,57 @@
 package ex01;
 
-public class ContaCorrente {
+public abstract class ContaCorrente {
     private float saldo;
     private int estado; // 1= conta ativa 2= conta inativa
     private int numConta;
     private int senha;
 
+    protected void setSaldo(float saldo){
+        this.saldo = saldo;
+    }
+
+    protected void setEstado(int pwd, int e){
+        if (senha == pwd)
+            estado = e;
+    }
+
+    protected void setNumConta(int numConta){
+        this.numConta = numConta;
+    }
+
+    protected void setSenha(int senha){
+        this.senha = senha;
+    }
+
+    protected boolean isSenha(int pwd){
+        if(senha == pwd)
+            return true;
+        else
+            return false;
+    }
+
+    public float getSaldo(int pwd){
+        if (senha == pwd)
+            return saldo;
+        else
+            return -1; // erro na senha
+    }
+    
+    protected int getEstado(int pwd) {
+        if (senha == pwd)
+            return estado;
+        else
+            return -1; // erro na senha
+    }
+
+    public abstract boolean debitaValor(float val, int pwd);
+    public abstract void debitaValor(float val);
+    public abstract void creditaValor(int pwd, float val);
+}
+
+
+
+    /* IMPLEMENTAÇAO ORIGINAL
     public ContaCorrente(float val, int num, int pwd) {
         numConta = num;
         senha = pwd;
@@ -63,4 +109,4 @@ public class ContaCorrente {
         else
             return false;
     }
-}
+    */
